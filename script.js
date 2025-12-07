7e20d82f8d83f13349b2b79de3f1b7676ec5d90c
Big-Geo <jpami030@gmail.com>
2025-12-07 11:06:43 +0100

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  const btn = document.getElementById("menu-btn");
+  const sidebar = document.getElementById("sidebar");
+
+  btn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    sidebar.classList.toggle("hidden");
+  });
+</script>
