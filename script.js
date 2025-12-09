70d3461371afa0b51908d744f04ceb293ae74a27
Big-Geo <jpami030@gmail.com>
2025-12-09 21:19:16 +0100

    updated

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (btn && sidebar) {
         btn.addEventListener("click", () => {
             sidebar.classList.toggle("open");
-            sidebar.classList.toggle("show");
         });
     }
 });
